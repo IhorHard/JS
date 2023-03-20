885a7a0512ead9385ec3661b0b7d496d93ecbb4f
IhorHard <IhorHardcorenko@gmail.com>
2023-03-20 13:45:28 +0200

    HW1.js

---
+++ HW1.js
@@ -0,0 +1,30 @@
+var item_1 = 5; 
+// console.log(item_1);
+var item_2 = 3; 
+// console.log(item_2);
+var item_3 = 8; 
+// console.log(item_3);
+var item_4 = "Yolochka";
+// console.log(item_4);
+// console.log(item_4 + item_3);
+// console.log(item_4 * item_3);
+var item_5 = item_3; 
+var item_6 = 15;
+var item_6_type = typeof item_6;
+// console.log("item_6_type == ", typeof item_6)
+// console.log("item_6_type == ", typeof item_6_type )
+
+var item_7 = String(item_7);
+// console.log(typeof item_7);
+
+var item_7_type = item_7;
+// console.log("item_7==", typeof item_7_type)
+
+var age_1 = 10;
+var age_2 = 18;
+var age_3 = 60;
+
+if
+
+
+
