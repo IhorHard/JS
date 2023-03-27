c5da2c690781982345e3100b7164f8881f7f2b91
IhorHardcorenko <124258993+IhorHard@users.noreply.github.com>
2023-03-27 22:14:00 +0300

    HW1.js

--- HW1.js
+++ HW1.js
@@ -1,16 +1,21 @@
 var item_1 = 5; 
 // console.log(item_1);
+
 var item_2 = 3; 
 // console.log(item_2);
+
 var item_3 = 8; 
 // console.log(item_3);
+
 var item_4 = "Yolochka";
 // console.log(item_4);
 // console.log(item_4 + item_3);
 // console.log(item_4 * item_3);
+
 var item_5 = item_3; 
 var item_6 = 15;
 var item_6_type = typeof item_6;
+
 // console.log("item_6_type == ", typeof item_6)
 // console.log("item_6_type == ", typeof item_6_type )
 
@@ -21,13 +26,16 @@ var item_7_type = item_7;
 // console.log("item_7==", typeof item_7_type)
 
 
- 
+// Создать переменную “age_1” и присвоить ей значение 10
+//  27. Создать переменную “age_2” и присвоить ей значение 18
+//  28. Создать переменную “age_3” и присвоить ей значение 60
 
 var age_1 = 10;
 var age_2 = 18;
 var age_3 = 60;
 
 // Создать if в котором будите проверять значение переменной age_1
+
 let age_1 = 25;
 
 if (age_1 >= 18) 
